accfb75a80d22c3fe23f5fa5b7ed84f044ceb012
thevaibhavshukla <vaibhav.shukla2910@gmail.com>
2023-12-27 18:01:55 +0530

    install and added cors

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const path = require('path')
+const cors = require('cors');
+
+
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
@@ -9,6 +12,7 @@ const mongodbConnection = require("./config/mongodbConnection");
 
 const port =process.env.PORT || 5000
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
